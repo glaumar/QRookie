1f37740f8d2ab874619adb8f77570b7a45c66cc5
Victor Wads <victor.laureano@gmail.com>
2024-06-18 16:28:20 -0300

    Revert "remove unused code"
    This reverts commit 9d478520530abd006c9438285d2178d2b6ddfac0.

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
 #include <QCoroQml>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
 
 #include "device_manager.h"
 #include "qrookie.h"
@@ -35,6 +36,13 @@ int main(int argc, char *argv[])
     QCoro::Qml::registerTypes();
 
     QQmlApplicationEngine engine;
+
+#if defined(Q_OS_MAC)
+    QString projectDir = QCoreApplication::applicationDirPath() + "/../Resources";
+    qDebug() << "Project Directory:" << projectDir; // Linha de debug
+    engine.addImportPath(projectDir + "/kirigami");
+#endif
+
     const QUrl url(u"qrc:/qt/qml/Main/main.qml"_qs);
     QObject::connect(
         &engine,
