a60b4a6fb758ab49b42ac8f124991810bc78fd39
glaumar <git@geekgo.tech>
2024-07-18 03:44:21 +0800

    chore: Bump version to v0.3.3

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
         {
           packages.default = pkgs.stdenv.mkDerivation {
             pname = "qrookie";
-            version = "0.3.1";
+            version = "0.3.3";
 
             src = ./.;
 
